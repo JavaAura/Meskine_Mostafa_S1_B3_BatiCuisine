ec666dc33b9ad725c4caf2d29c4cccf4ec535854
MesVortex <meskinemostafa4@gmail.com>
2024-09-19 10:03:40 +0100

    added main menu to consoleUI

--- BatiCuisine_structure/src/org/BatiCuisine/Main.java
+++ BatiCuisine_structure/src/org/BatiCuisine/Main.java
@@ -1,9 +1,10 @@
 package org.BatiCuisine;
 
 import org.BatiCuisine.Database.Migration;
+import org.BatiCuisine.View.ConsoleUI;
 
 public class Main {
     public static void main(String[] args) {
-
+        new ConsoleUI();
     }
 }
--- BatiCuisine_structure/src/org/BatiCuisine/View/ConsoleUI.java
+++ BatiCuisine_structure/src/org/BatiCuisine/View/ConsoleUI.java
@@ -1,4 +1,40 @@
 package org.BatiCuisine.View;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class ConsoleUI {
+    public static Scanner scan = new Scanner(System.in);
+//    private static final Logger logger = Logger.getLogger(ConsoleUI.class.getName());
+
+    public ConsoleUI() {
+        while (true) {
+            mainMenu();
+            int choice = scan.nextInt();
+            switch (choice) {
+                case 1 -> {
+
+                }
+                case 2 -> {
+                    System.out.println("Afficher les projets existants");
+                }
+                case 3 -> {
+                    System.out.println("Calculer le coût d'un projet");
+                }
+                default -> {
+                    return;
+                }
+            }
+        }
+    }
+
+    public void mainMenu() {
+        System.out.println("=== Welcome to the kitchen renovation project management application ===");
+        System.out.println("=== Main Menu ===");
+        System.out.println("1.Create a new project");
+        System.out.println("2.Show All projects");
+        System.out.println("3.Calculate the cost of a project");
+        System.out.println("4.Exit");
+        System.out.println("=> ");
+    }
 }
