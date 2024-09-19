a7fbd466340c0e1a4d73fda8788d61e5c60a7836
MesVortex <meskinemostafa4@gmail.com>
2024-09-19 11:05:04 +0100

    added getAll method to DAO

--- BatiCuisine_structure/src/org/BatiCuisine/Dao/Interfaces/DAO.java
+++ BatiCuisine_structure/src/org/BatiCuisine/Dao/Interfaces/DAO.java
@@ -1,5 +1,6 @@
 package org.BatiCuisine.Dao.Interfaces;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface DAO<T> {
@@ -10,4 +11,6 @@ public interface DAO<T> {
     void update(T t);
 
     boolean delete(UUID id);
+
+    List<T> getAll();
 }
