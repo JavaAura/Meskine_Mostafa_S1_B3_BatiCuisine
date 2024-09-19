f9f0bb6be28a0becdd13b1e9d43b820e1c150952
MesVortex <meskinemostafa4@gmail.com>
2024-09-19 11:57:41 +0100

    updated clientRepository & clientRepositoryImpl

--- BatiCuisine_structure/src/org/BatiCuisine/Repository/Impl/ClientRepositoryImpl.java
+++ BatiCuisine_structure/src/org/BatiCuisine/Repository/Impl/ClientRepositoryImpl.java
@@ -1,7 +1,42 @@
 package org.BatiCuisine.Repository.Impl;
 
+import org.BatiCuisine.Dao.Interfaces.ClientDao;
+import org.BatiCuisine.Model.Client;
 import org.BatiCuisine.Repository.Interfaces.ClientRepository;
+
+import java.util.List;
+import java.util.UUID;
 
 public class ClientRepositoryImpl implements ClientRepository {
 
+    private final ClientDao clientDao;
+
+    public ClientRepositoryImpl(ClientDao clientDao) {
+        this.clientDao = clientDao;
+    }
+
+    @Override
+    public void addClient(Client client) {
+        clientDao.create(client);
+    }
+
+    @Override
+    public Client getClientById(UUID id) {
+        return clientDao.read(id);
+    }
+
+    @Override
+    public void updateClient(Client client) {
+        clientDao.update(client);
+    }
+
+    @Override
+    public boolean removeClient(UUID id) {
+        return clientDao.delete(id);
+    }
+
+    @Override
+    public List<Client> getAllClients() {
+        return clientDao.getAll();
+    }
 }
--- BatiCuisine_structure/src/org/BatiCuisine/Repository/Interfaces/ClientRepository.java
+++ BatiCuisine_structure/src/org/BatiCuisine/Repository/Interfaces/ClientRepository.java
@@ -1,4 +1,18 @@
 package org.BatiCuisine.Repository.Interfaces;
 
+import org.BatiCuisine.Model.Client;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface ClientRepository {
+    void addClient(Client client);
+
+    Client getClientById(UUID id);
+
+    void updateClient(Client client);
+
+    boolean removeClient(UUID id);
+
+    List<Client> getAllClients();
 }
