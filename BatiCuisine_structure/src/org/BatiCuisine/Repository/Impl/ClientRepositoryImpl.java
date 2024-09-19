92a87c5332b72a4639212244766f4aa53f8c92f2
MesVortex <meskinemostafa4@gmail.com>
2024-09-19 10:22:56 +0100

    added client service

---
+++ BatiCuisine_structure/src/org/BatiCuisine/Repository/Impl/ClientRepositoryImpl.java
@@ -0,0 +1,6 @@
+package org.BatiCuisine.Repository.Impl;
+
+import org.BatiCuisine.Repository.Interfaces.ClientRepository;
+
+public class ClientRepositoryImpl implements ClientRepository {
+}
---
+++ BatiCuisine_structure/src/org/BatiCuisine/Repository/Interfaces/ClientRepository.java
@@ -0,0 +1,4 @@
+package org.BatiCuisine.Repository.Interfaces;
+
+public interface ClientRepository {
+}
